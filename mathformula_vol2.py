cdc1f30d08fa25b9d8c999fbf37c2274c64dfd24
Evstenia <evstenia@gmail.com>
2024-09-28 21:50:39 +0200

    Delete mathformula_vol2.py

--- mathformula_vol2.py
+++
@@ -1,5 +0,0 @@
-a=float(input('Введите значение a='))
-b=float(input('Введите значение b='))
-c=float(input('Введите значение c='))
-d=float(input('Введите значение d='))
-print(((-3+a**2)*b-2**3)/c-2*d)
